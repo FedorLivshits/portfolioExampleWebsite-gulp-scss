84e44964a5170e347cb0b419387b190b27a47c55
FedorLivshits <fediliv@gmail.com>
2021-06-20 17:33:25 +0300

    added burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
-$(function () {
+window.addEventListener('load', () => {
+    const menuList = document.querySelector('.header__menu-list');
+    const burgerMenu = document.querySelector('.header__burger');
+    const body = document.querySelector('body')
 
+    burgerMenu.addEventListener('click', () => {
+        menuList.classList.toggle('header__menu-list--active');
+        burgerMenu.classList.toggle('header__burger--active');
+        body.classList.toggle('lock')
+    })
 })
